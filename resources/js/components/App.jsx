f0dd6281c2b43f53374e242db6a5abfcd82f368c
romaindepret91 <romaindepret91@gmail.com>
2022-08-30 11:53:35 -0400

    initial commit: setup of application

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+    return (
+        <React.Fragment>
+            <div className="App-header">
+                <h1>Hello World</h1>
+            </div>
+        </React.Fragment>
+    );
+}
+
+export default App;
