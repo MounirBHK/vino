e3db187163658602f9b50e976e1e195130d1ccc0
romaindepret91 <romaindepret91@gmail.com>
2022-09-20 12:24:43 -0400

    Interface admin: Message d'erreur et de confirmation à la suppression d'une bouteille

--- resources/js/components/admin/AdminHome.jsx
+++ resources/js/components/admin/AdminHome.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 import { useEffect } from "react";
 import "./AdminHome.scss";
-import { Admin, Resource, fetchUtils } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import { useNavigate } from "react-router-dom";
 import Dashboard from "./Dashboard";
 import Bouteilles from "./bouteilles/Bouteilles";
 import Users from "./users/Users";
 import authProvider from "./authProvider";
-import simpleRestProvider from "ra-data-simple-rest";
+import { dataProvider } from "./dataProvider";
 
 function AdminHome({ setBouteilles, bouteilles }) {
     const navigate = useNavigate();
     const userAdmin = JSON.parse(localStorage.getItem("adminUser")) || null;
-
-    const httpClient = (url, options = {}) => {
-        const token = userAdmin["access_token"] || null;
-        if (!options.headers) {
-            options.headers = new Headers({ Accept: "application/json" });
-        }
-        options.headers.set("Authorization", `Bearer ${token}`);
-        return fetchUtils.fetchJson(url, options);
-    };
-
-    const dataProvider = simpleRestProvider(
-        window.location.origin + "/api",
-        httpClient
-    );
 
     useEffect(() => {
         const adminUser = localStorage.getItem("adminUser") || null;
--- resources/js/components/admin/bouteilles/CustomDeleteBtn.jsx
+++ resources/js/components/admin/bouteilles/CustomDeleteBtn.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-import { BulkDeleteButton } from "react-admin";
+import { dataProvider } from "../dataProvider";
+import { useNotify, Button, useUnselectAll } from "react-admin";
 
-function CustomDeleteBtn({ setBouteilles, bouteilles, selectedIds }) {
-    return (
-        <BulkDeleteButton
-            onClick={() => {
-                const newBouteilles = bouteilles.filter(
-                    (bouteille) => !selectedIds.includes(bouteille.id)
+function CustomDeleteBtn({ setBouteilles, bouteilles, resource, selectedIds }) {
+    const notify = useNotify();
+    const unselectAll = useUnselectAll(resource);
+    const handleClick = () => {
+        dataProvider
+            .deleteMany("bouteilles", { ids: selectedIds })
+            .then((response) => {
+                const nbBouteillesSupp = response.data.length;
+                notify(
+                    `Suppression de ${nbBouteillesSupp} bouteille(s)  réussie`
                 );
-                setBouteilles(newBouteilles);
-            }}
-        >
-            Delete
-        </BulkDeleteButton>
-    );
+                unselectAll();
+            })
+            .catch((error) => {
+                if (error.status == 405) {
+                    notify(
+                        "Suppression impossible: la ressource est utilisée par les usagers"
+                    );
+                    unselectAll();
+                }
+            });
+    };
+    return <Button label="Supprimer" onClick={handleClick}></Button>;
 }
 
 export default CustomDeleteBtn;
